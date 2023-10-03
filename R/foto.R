84b88c0195b1d0139090d7184637fddf1eec7fbe
Koen Hufkens <koen.hufkens@gmail.com>
2023-10-03 21:50:08 +0000

    version 1.1

--- R/foto.R
+++ R/foto.R
@@ -1,201 +1,221 @@
 #' Calculates FOTO classification of texture
 #'
-#' Note that the input matrix should be square or results will be discarded 
+#' Note that the input matrix should be square or results will be discarded
 #  the output is an increment to i (global) and data that is
-#  written matrix 'output' (global) to be defined up front. 
+#  written matrix 'output' (global) to be defined up front.
 #'
-#' @param x an image file, raster layer, stack or brick 
-#' for multilayered images (RGB or otherwise) data are averaged to 
-#' a single layer raster
+#' @param x an image file, or single or multi-layer SpatRaster
+#' (RGB or otherwise), multi-layer data are averaged to a single layer
 #' @param window_size a moving window size in pixels (default = 61 pixels)
-#' @param plot plot output, bolean \code{TRUE} or \code{FALSE}
 #' @param method zones (for discrete zones) or mw for a moving window
 #' approach
+#' @param pca execute PCA, \code{TRUE} or \code{FALSE}. If \code{FALSE} only
+#' the radial spectra are returned for additional manipulation. Plotting is
+#' ignored if set to \code{FALSE}.
 #' @param norm_spec normalize radial spectrum,
 #' bolean \code{TRUE} or \code{FALSE}
+#' @param high_pass apply high pass filter to radial spectra,
+#' bolean \code{TRUE} or \code{FALSE}
+#' @param plot plot output, bolean \code{TRUE} or \code{FALSE}
 #' @return returns a radial spectrum for a moving window across a
 #' raster layer
-#' @keywords foto, radial spectrum
 #' @seealso \code{\link[foto]{rspectrum}}
-#' @importFrom raster brick
 #' @export
 #' @examples
-#'
-#' \donttest{
+#' \dontrun{
 #' # load demo data
-#' r <- raster::raster(system.file("extdata", "yangambi.png", package = "foto",
-#'      mustWork = TRUE))
-#' 
+#' r <- terra::rast(system.file("extdata",
+#'   "yangambi.png",
+#'   package = "foto",
+#'   mustWork = TRUE
+#' ))
+#'
 #' # classify pixels using zones (discrete steps)
 #' output <- foto(r,
-#'                plot = TRUE,
-#'                window_size = 25,
-#'                method = "zones")
-#' 
+#'   plot = FALSE,
+#'   window_size = 25,
+#'   method = "zones"
+#' )
+#'
 #' # print data structure
 #' print(names(output))
-#'}
-
-
-foto <- function(
-  x,
-  window_size = 61,
-  method = "zones",
-  plot = FALSE,
-  norm_spec = TRUE
-  ){
+#' }
+#'
+foto <- function(x,
+                 window_size = 61,
+                 method = "zones",
+                 norm_spec = FALSE,
+                 high_pass = TRUE,
+                 pca = TRUE,
+                 plot = FALSE) {
   
   # get the current enviroment
   env <- environment()
-  
+
   # check for image
-  if(missing(x)){
+  if (missing(x)) {
     stop("No image or RasterLayer / Stack or Brick provided.")
   }
-  
+
   # read the image / matrix file and apply extract the r-spectra
-  if (!grepl("Raster",class(x))){
-    
+  if (!grepl("SpatRaster", class(x))) {
     # check file
-    if(!file.exists(x)){
+    if (!file.exists(x)) {
       stop("Incorrect image path.")
     }
-    
+
     # if not a raster object read as image
-    img <- raster::brick(x)
-    
+    img <- suppressWarnings(terra::rast(x))
+
     # calculate the mean (grayscale)
     # image if there is more than one
     # image band
-    img <- raster::mean(img)
+    img <- terra::mean(img)
   }
-  
-  if (class(x)[1]=="RasterLayer"){
-    img <- x 
+
+  if (inherits(x, "SpatRaster")) {
+    if (dim(x)[3] > 1) {
+      img <- terra::mean(x)
+    } else {
+      img <- x
+    }
   }
-  
-  if (class(x)[1]=="RasterBrick" ||
-      class(x)[1]=="RasterStack"){
-    
-    # calculate the mean (grayscale)
-    # image if there is more than one
-    # image band
-      img <- raster::mean(x)
-  }
-  
+
   if(method == "zones" || method == "mw"){
     if(method=="zones"){
       # get number of cells to be aggregated to
-      N <- ceiling(img@nrows/window_size)
-      M <- ceiling(img@ncols/window_size)
+      N <- ceiling(nrow(img)/window_size)
+      M <- ceiling(ncol(img)/window_size)
       cells <- N*M
     }else{
       # get number of cells to be aggregated to
-      N <- img@nrows
-      M <- img@ncols
+      N <- nrow(img)
+      M <- ncol(img)
       cells <- N*M
     }
   }else{
     print("Choose an available method: zones or mw")
   }
-  
+
   # use assign
   # define output matrix and global increment, i
-  if( window_size/2 < 29){
-    output <- matrix(0, cells, window_size/2)
-  }else{
+  if (window_size / 2 < 29) {
+    output <- matrix(0, cells + 1, window_size / 2)
+  } else {
     output <- matrix(0, cells, 29)
   }
-  
+
   # see rspectrum function above
   i <- 0
-  
+
   # for every zone execute the r-spectrum function
-  if(method=="zones"){
-    zones <- raster::aggregate(
-      img,
-      fact = window_size,
-      fun = function(x, ...) {
-        rspectrum(
-          x = x,
-          w = window_size,
-          n = norm_spec,
-          env = env
-        )
-      },
-      expand = TRUE,
-      na.rm = TRUE
-    )
-  }else{
+  if (method == "zones") {
+    zones <- terra::aggregate(
+        img,
+        fact = window_size,
+        fun = function(x, ...) {
+          rspectrum(
+            x = x,
+            w = window_size,
+            n = norm_spec,
+            h = high_pass,
+            env = env
+          )
+        },
+        na.rm = TRUE
+      )
+  } else {
     message("A moving window approach is computationally intensive.")
     message("This might take a while.")
-    zones <- raster::focal(
-      img,
-      matrix(
-        rep(1, window_size * window_size),
-        ncol = window_size,
-        nrow = window_size
-      ),
-      fun = function(x, ...) {
-        rspectrum(
-          x = x,
-          w = window_size,
-          n = norm_spec,
-          env = env
-        )
-      },
-      expand = TRUE,
-      na.rm = TRUE
-    )
+    zones <- terra::focal(
+        img,
+        matrix(
+          rep(1, window_size * window_size),
+          ncol = window_size,
+          nrow = window_size
+        ),
+        fun = function(x, ...) {
+          rspectrum(
+            x = x,
+            w = window_size,
+            n = norm_spec,
+            env = env
+          )
+        },
+        expand = FALSE,
+        na.rm = TRUE
+      )
   }
-  
+
   # 3. reformat the r-spectrum output (normalize) and apply a PCA
   # set all infinite values to NA
   output[is.infinite(output)] <- NA
-  
-  # Normalize matrix column wise (ignoring NA values)
-  noutput <- suppressWarnings(base::scale(output))
-  
-  # set NA/Inf values to 0 as the pca analys doesn't take NA values
-  noutput[is.infinite(noutput) | is.na(noutput)] <- 0
-  
-  # the principal component analysis
-  pcfit <- stats::princomp(noutput)
-  
-  # create reclass files based upon PCA scores
-  # only the first 3 PC will be considered
-  for (i in 1:3){
-    assign(paste('rcl.',i,sep=''),
-           cbind(seq(1, length(pcfit$scores[,i]), 1),
-                 normalize(pcfit$scores[,i])),
-           envir = env)
-  }  
-  
-  # reclassify using the above reclass files
-  PC <- lapply(1:3, function(i){
-    return(raster::reclassify(zones,
-                              get(paste('rcl.',i,sep=''))))
-  })
-  
-  # create a raster brick
-  img_RGB <- do.call("brick", PC)
-  
-  # plot the classification using an RGB representation of the first 3 PC
-  if (plot){
-    raster::plot(img,
-                 col = grDevices::gray(0:100/100),
-                 legend = FALSE,
-                 box = FALSE,
-                 axes = FALSE)
-    raster::plotRGB(img_RGB,
-                    stretch = 'hist',
-                    add = TRUE, 
-                    alpha = 128,
-                    bgalpha = 0)
+
+  if (pca) {
+    # Normalize matrix column wise (ignoring NA values)
+    noutput <- suppressWarnings(base::scale(output))
+
+    # set NA/Inf values to 0 as the pca analys doesn't take NA values
+    noutput[is.infinite(noutput) | is.na(noutput)] <- 0
+
+    # find the location of all zero rows (empty)
+    zero_location <- apply(noutput, 1, function(x) all(x == 0))
+
+    # the principal component analysis
+    pcfit <- stats::princomp(noutput)
+
+    # set empty (zeros) rows to NA
+    pcfit$scores[which(zero_location), ] <- NA
+
+    # create reclass files based upon PCA scores
+    # only the first 3 PC will be considered
+    for (i in 1:3) {
+      assign(paste("rcl.", i, sep = ""),
+        cbind(
+          seq(1, nrow(pcfit$scores), 1),
+          normalize(pcfit$scores[, i])
+        ),
+        envir = env
+      )
+    }
+    
+    # reclassify using the above reclass files
+    PC <- lapply(1:3, function(i) {
+      return(
+          terra::classify(
+            zones,
+            get(paste("rcl.", i, sep = ""))
+          )
+        )
+    })
+
+    # create a raster brick
+    img_RGB <- suppressWarnings(do.call("c", PC))
+
+    # plot the classification using an RGB representation of the first 3 PC
+    if (plot) {
+      terra::plot(img,
+        col = grDevices::gray(0:100 / 100),
+        legend = FALSE,
+        axes = FALSE
+      )
+      terra::plotRGB(
+        img_RGB,
+        add = TRUE,
+        scale = 255,
+        stretch = "hist",
+        bgalpha = 0,
+        alpha = 0.5
+      )
+    }
+  } else {
+    img_RGB <- NULL
   }
-  
+
   # return data
-  return(list("zones" = zones, 
-              "radial_spectra" = output,
-              "rgb" = img_RGB))
+  return(list(
+    "zones" = zones,
+    "radial_spectra" = output,
+    "rgb" = img_RGB
+  ))
 }
--- inst/doc/foto-vignette.R
+++ inst/doc/foto-vignette.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -6,34 +6,61 @@ knitr::opts_chunk$set(
 
 # load the library
 library(foto)
-library(raster)
-
+library(terra)
 
 ## ----figure_1, message = FALSE, warning = FALSE, fig.width=7, fig.height=7----
 library(foto)
+library(terra)
 
 # load demo data
-r <- raster::raster(sprintf("%s/extdata/yangambi.png", path.package("foto")))
+r <- terra::rast(sprintf("%s/extdata/yangambi.png", path.package("foto")))
 
 # classify pixels using zones (discrete steps)
 output <- foto(r,
      plot = TRUE,
-     window_size = 25,
-     method = "zones")
+     window_size = 51,
+     method = "zones"
+     )
 
-# print data structure
-print(names(output))
-
-## ----figure_2, fig.width=7, fig.height=7---------------------------------
+## ----figure_2, fig.width=7, fig.height=7--------------------------------------
 # crop the image for speed
-r <- crop(r, extent(1,100,1,100))
+r_1 <- terra::crop(r, terra::ext(1,100,1,100))
 
 # crop the image
-output <- foto(r,
+output_1 <- foto(r_1,
      plot = TRUE,
-     window_size = 25,
+     window_size = 51,
      method = "mw")
 
-# print data structure
-print(names(output))
+## ----warning=FALSE, message=FALSE---------------------------------------------
+# load demo data path
+path <- system.file("extdata",package="foto")
+
+# classify pixels using zones (discrete steps)
+# across images
+output_batch <- foto_batch(path = path,
+                window_size = 51,
+                method = "zones")
+
+# indiviual images would look different when processed one by one
+r_2 <- terra::rast(sprintf("%s/extdata/yangambi_2.png",
+                              path.package("foto")))
+
+# classify pixels using zones (discrete steps)
+output_2 <- foto(r_2,
+     window_size = 51,
+     method = "zones")
+
+## ----figure_3, fig.width=7, fig.height=4--------------------------------------
+par(mfrow = c(1,2))
+plot(r_2, col = grey.colors(100), axes = FALSE, legend = FALSE, box = FALSE)
+plot(r, col = grey.colors(100), axes = FALSE, legend = FALSE, box = FALSE)
+
+par(mfrow = c(1,2))
+plotRGB(output_batch[[1]], stretch = "hist")
+plotRGB(output_batch[[2]], stretch = "hist")
+
+par(mfrow = c(1,2))
+plotRGB(output_2$rgb, stretch = "hist")
+plotRGB(output$rgb, stretch = "hist")
 
